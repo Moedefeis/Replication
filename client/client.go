fe225ef5e55e29d85541c763c6b38175f3af7977
Mathias Hvolgaard Darling Larsen <mhvl@itu.dk>
2022-11-23 14:26:26 +0100

    replication implemented

--- client/client.go
+++ client/client.go
@@ -1,5 +1,83 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 
+	auction "github.com/Moedefeis/Replication/grpc"
+	"google.golang.org/grpc"
+)
+
+var conns = make(map[int]auction.AuctionClient)
+var reader *bufio.Scanner
+var ctx = context.Background()
+
+func main() {
+	reader = bufio.NewScanner(os.Stdin)
+
+	for i := 1; i < len(os.Args); i++ {
+		port, _ := strconv.Atoi(os.Args[i])
+
+		conn, err := grpc.Dial(fmt.Sprintf(":%v", port), grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("Could not connect: %s", err)
+		}
+		defer conn.Close()
+		conns[port] = auction.NewAuctionClient(conn)
+	}
+
+	handleInput()
+}
+
+func handleInput() {
+	for {
+		reader.Scan()
+		input := strings.ToLower(reader.Text())
+		if amount, err := strconv.Atoi(input[4:]); err != nil && strings.Contains(input, "bid") {
+			bid(amount)
+		} else if strings.Contains(input, "result") {
+			queryResult()
+		}
+	}
+}
+
+func bid(amount int) {
+	success := true
+	bid := &auction.Amount{Amount: int32(amount)}
+	for port, conn := range conns {
+		response, err := conn.Bid(ctx, bid)
+		if err != nil {
+			handleCrashedServer(port)
+		} else {
+			success = success && response.Status
+		}
+	}
+	if success {
+		log.Printf("Bidded %d successfully", amount)
+	} else {
+		log.Printf("Bidded %d unsuccessfully, as higher bid existed", amount)
+	}
+}
+
+func queryResult() {
+	var result int32 = 0
+	for port, conn := range conns {
+		highestBid, err := conn.Result(ctx, &auction.Void{})
+		if err != nil {
+			handleCrashedServer(port)
+		} else if highestBid.Amount > result {
+			result = highestBid.Amount
+		}
+	}
+	log.Printf("Highest bid is: %d", result)
+}
+
+func handleCrashedServer(port int) {
+	log.Printf("Server at port: %d crashed", port)
+	delete(conns, port)
 }
--- server/server.go
+++ server/server.go
@@ -1,5 +1,59 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"sync"
 
+	auction "github.com/Moedefeis/Replication/grpc"
+	"google.golang.org/grpc"
+)
+
+type Auction struct {
+	auction.UnimplementedAuctionServer
+
+	highestBid int32
+	bidLock    sync.Mutex
+}
+
+func main() {
+	port, _ := strconv.Atoi(os.Args[1])
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%v", port))
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9080: %v", err)
+	}
+
+	defer listener.Close()
+	a := &Auction{
+		highestBid: 0,
+	}
+
+	server := grpc.NewServer()
+	auction.RegisterAuctionServer(server, a)
+
+	if err := server.Serve(listener); err != nil {
+		log.Fatalf("failed to server %v", err)
+	}
+}
+
+func (a *Auction) Bid(ctx context.Context, bid *auction.Amount) (*auction.Response, error) {
+	var status bool
+	a.bidLock.Lock()
+	if a.highestBid < bid.Amount {
+		a.highestBid = bid.Amount
+		status = true
+	} else {
+		status = false
+	}
+	a.bidLock.Unlock()
+	return &auction.Response{Status: status}, nil
+}
+
+func (a *Auction) Result(ctx context.Context, void *auction.Void) (*auction.Amount, error) {
+	return &auction.Amount{Amount: a.highestBid}, nil
 }
